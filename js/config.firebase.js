c799ae968fc6f2be5dbf4a5073d3ba9138b1c613
Arif Solmaz <arif.solmaz@gmail.com>
2026-04-06 03:04:10 +0300

    Add Firebase and app configuration settings

---
+++ js/config.firebase.js
@@ -0,0 +1,13 @@
+window.__FIREBASE_CONFIG__ = {
+  apiKey: "AIzaSyBuZoTgNz1TeR2wkQ_lNcGcNe3vEw_2pT8",
+  authDomain: "tarih-vakfi-f31e0.firebaseapp.com",
+  projectId: "tarih-vakfi-f31e0",
+  storageBucket: "tarih-vakfi-f31e0.firebasestorage.app",
+  messagingSenderId: "555807333352",
+  appId: "1:555807333352:web:259039332f809c91d4e25d"
+};
+
+window.__APP_CONFIG__ = {
+  admins: ["arif.solmaz@gmail.com"],
+  appName: "Tarih Vakfı Gönüllü Takip Sistemi"
+};
